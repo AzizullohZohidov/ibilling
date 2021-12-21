7646c6d0ade5920de15d2cf1b3c68475d7a4c8dd
AzizullohZohidov <zohidov.azizulloh@gmail.com>
2021-12-21 19:10:00 +0500

    Project restructured

---
+++ lib/presentation/router/app_router.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  /*static Route onGenerateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case RouteConstants.signUpScreen:
+        return MaterialPageRoute(builder: (_) => SignUpScreen());
+        break;
+      default:
+    }
+  }*/
+}
+
+class RouteConstants {
+  //static const signUpScreen = '/sign-up-screen';
+}
